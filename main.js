5da134423074c889a842e2e265fee744c01ecfd9
keepthinkingit <781130764@qq.com>
2019-02-24 17:10:00 +0800

    09 vue component exercise

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
+Vue.component('message', {
+    props: ['title', 'body'],
 
-    // data() {
-    //     return {
-    //         tasks: [
-    //             {task: 'Go to the store', complete: true},
-    //             {task: 'Go to farm', complete: false},
-    //             {task: 'Go to work', complete: true},
-    //         ]
-    //     }
-    // }
+    data() {        //only return object
+        return {    //页面渲染时传递初始属性值
+            isVisible: true
+        }
+    },
+
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                {{ title }}
+            </div>
+            
+            <!--点击时改变 isVisible 属性-->
+            <button type="button" @click="hideModel">X</button> 
+            <!--第二种方法(简易书写) -->
+            <!--<button type="button" @click="isVisible=false">X</button>-->
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+`,
+
+
+methods: {
+    hideModel(){
+        this.isVisible = false;
+    }
+}
+
 });
 
 new Vue({
-    el: '#root'
-
-})
+    el: '#root',
+});
