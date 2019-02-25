d27dabba161405c458cdb497e68f528f9b0b1f03
keepthinkingit <781130764@qq.com>
2019-02-25 14:25:28 +0800

    15. vue inline-template

--- main.js
+++ main.js
@@ -1,42 +1,15 @@
 
-Vue.component('modal', {
-    template: `
-    <div class="modal is-active">
-    
-      <div class="modal-background"></div>
-      
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">
-          <slot name="header"></slot>     <!--动态生成-->
-          </p>
-          
-          <button class="delete" aria-label="close"></button>
-        </header>
-        
-        <section class="modal-card-body">
-          <!-- Content ... -->
-          <slot>
-            <!--Default content here. &lt;!&ndash;可以写一些默认内容在此&ndash;&gt;-->
-          </slot>
-        </section>
-        
-        <footer class="modal-card-foot">
-            <slot name="footer"></slot>
-        </footer>
-        
-      </div>
-    </div>
-`,
+Vue.component('progress-view', {
 
-    methods: {
-    }
+    data() {
+        return { completionRate: 50 };
+    },
+
 });
 
 new Vue({
     el: '#root',
     data: {
-        couponApplied: false,
     },
 
 });
