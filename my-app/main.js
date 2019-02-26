2225ff6f7283f4185abc53b041e0f49a9868b071
keepthinkingit <781130764@qq.com>
2019-02-26 23:53:57 +0800

    25. custom input components

--- my-app/main.js
+++ my-app/main.js
@@ -1,120 +1,32 @@
-// import Vue from 'vue'
+Vue.component('coupon', {
+  props: ['code'],
 
-// import App from './App.vue'
-
-class Errors {
-  constructor() {
-    this.errors = {};
-  }
-
-  has(field) {
-    return this.errors.hasOwnProperty(field);
-  }
-
-  any() {
-    return Object.keys(this.errors).length > 0;
-  }
-
-  get(field) {
-    if (this.errors[field]) {
-      return this.errors[field[0]];
-    }
-  }
-
-  record(errors) {
-    this.errors = errors;
-  }
-
-  clear(field) {
-    if (field) {
-      delete this.errors[field];
-      return;
-    }
-
-    this.errors = {};
-  }
-}
-
-class Form {
-  constructor(data) {
-    this.originalData = data;
-
-    for (let field in data) {
-      this[field] = data[field];
-    }
-
-    this.errors = new Errors();
-
-  }
+  template: `
+    <input type="text" :value="code" @input="updateCode($event.target.value)" ref="input">
+    `,
 
   data() {
-    let data = {};
-
-    for( let property in this.originalData){
-      data[property] = this[property];
+    return {
+      invalids: ['free', 'work']
     }
+  },
 
-    return data;
-  }
+  methods: {
+    updateCode(code){
+      if(this.invalids.includes(code)){
+        alert('this code is no longer valid');
 
-  reset() {
-    for (let field in this.originalData) {
-      this[field] = '';
+        this.$refs.input.value = code = '';
+      }
+      this.$emit('input', code);
+      return;
     }
-
-    this.reset();
   }
-
-  post(url){
-    return this.submit('POST',url);
-  }
-
-  delete(url){
-    return this.submit('DELETE',url);
-  }
-
-  submit(requestType, url) {
-    return new Promise((resolve,reject) =>{
-      axios[requestType](url, this.data())
-        .then(response => {
-          this.onSuccess(response.data);
-
-          resolve(response.data);
-        })
-        .catch(error =>{
-          this.onFail(error.response.data);
-
-          reject(error.response.data);
-        })
-    })
-  }
-
-  onSuccess(data) {
-    alert(data.message);
-
-    this.errors.clear();
-
-  }
-
-  onFail(errors) {
-    this.errors.record(errors);
-  }
-}
-
+})
 new Vue({
   el: '#app',
 
   data: {
-    form: new Form({
-      name: '',
-      description: '',
-    })
-  },
-
-  methods: {
-    onSubmit() {
-      this.form.delete('/create');
-    }
+    coupon: 'free lunch?'
   }
-
-});
+})
